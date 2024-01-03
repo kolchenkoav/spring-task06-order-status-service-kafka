e0e37c4e14feb84615968e004381e86a7feccd4e
Aleksey <kolchenkoav@gmail.com>
2024-01-03 08:12:56 +0300

    first init

---
+++ src/main/java/com/example/orderstatusservice/OrderStatusServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.orderstatusservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderStatusServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderStatusServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/orderstatusservice/OrderStatusServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.orderstatusservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderStatusServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
